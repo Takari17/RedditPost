61c22f432923fcf28175b70d828e6c315e7bdbe0
Takari Harrison <takariharrison@gmail.com>
2019-06-06 18:59:35 -0400

    Reduce repetitive code in CommonUtils.kt

--- app/src/main/java/com/example/redditpost/utils/CommonUtils.kt
+++ app/src/main/java/com/example/redditpost/utils/CommonUtils.kt
@@ -1,22 +1,23 @@
 package com.example.redditpost.utils
 
-import com.example.redditpost.data.remote.PostData
+import com.example.redditpost.data.remote.RedditResponse
 
-fun List<PostData>.getTitleAuthor() = this[0].data.children[0].data.author
+private fun getTitle(list: List<RedditResponse>) = list[0].data.children[0].data
 
-fun List<PostData>.getCommentAuthor(index: Int) = this[1].data.children[index].data.author
+private fun getComment(list: List<RedditResponse>, index: Int) = list[1].data.children[index]
 
-fun List<PostData>.getTitleText() = this[0].data.children[0].data.titleText
+fun List<RedditResponse>.getTitleAuthor() = getTitle(this).author
 
-fun List<PostData>.getTitleUpVotes() = this[0].data.children[0].data.titleUpVotes.toString()
+fun List<RedditResponse>.getTitleText() = getTitle(this).titleText
 
-fun List<PostData>.getRepliesAmount() = this[0].data.children[0].data.repliesAmount.toString()
+fun List<RedditResponse>.getTitleUpVotes() = getTitle(this).titleUpVotes.toString()
 
-fun List<PostData>.getCommentUpVotes(index: Int) = this[1].data.children[index].data.commentUpVotes
+fun List<RedditResponse>.getRepliesAmount() = getTitle(this).repliesAmount.toString()
 
-fun List<PostData>.getCommentText(index: Int) = this[1].data.children[index].data.commentText
+fun List<RedditResponse>.getCommentAuthor(index: Int) = this[1].data.children[index].data.author
 
-fun List<PostData>.getCommentAmount() = this[1].data.children.size
+fun List<RedditResponse>.getCommentUpVotes(index: Int) = getComment(this, index).data.commentUpVotes.toString()
 
+fun List<RedditResponse>.getCommentText(index: Int) = getComment(this, index).data.commentText
 
-
+fun List<RedditResponse>.getCommentAmount() = this[1].data.children.size
