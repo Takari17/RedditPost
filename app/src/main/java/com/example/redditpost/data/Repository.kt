ebe84a68f8c592458e4a2024b59f0ee964a5e056
Takari Harrison <takariharrison@gmail.com>
2019-06-06 18:51:06 -0400

    Inject the Repositories dependencies

--- app/src/main/java/com/example/redditpost/data/Repository.kt
+++ app/src/main/java/com/example/redditpost/data/Repository.kt
@@ -1,13 +1,9 @@
 package com.example.redditpost.data
 
-import android.app.Application
-import com.example.redditpost.data.remote.ServiceGenerator
+import com.example.redditpost.data.remote.RedditApi
 import io.reactivex.schedulers.Schedulers
 
-class Repository(application: Application) {
-
-    private val serviceGenerator = ServiceGenerator(application)
-    private val redditApi = serviceGenerator.redditApi
+class Repository(redditApi: RedditApi) {
 
     val getAllPostData = redditApi.getAllPostData()
         .subscribeOn(Schedulers.io())
