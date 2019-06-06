b57e801dbabd12c88f10abf07d8d17ce93511b34
Takari Harrison <takariharrison@gmail.com>
2019-06-06 18:59:10 -0400

    Create a DataSource class that holds an enum for the Url's

---
+++ app/src/main/java/com/example/redditpost/utils/DataSource.kt
@@ -0,0 +1,7 @@
+package com.example.redditpost.utils
+
+enum class Url(val text: String){
+    BASE_URL("https://www.reddit.com"),
+
+    FULL_URL("https://www.reddit.com/r/androiddev/comments/b877eq/devs_of_reddit_what_is_something_that_you_learned/")
+}
